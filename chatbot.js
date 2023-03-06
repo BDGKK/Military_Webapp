3070128eac4f0129abfa9886468a9a77685cd812
rekredhead <rekredhead@gmail.com>
2023-03-06 11:31:27 +0530

    Made nicer UI in chatbot HTML

--- chatbot.js
+++ chatbot.js
@@ -3,12 +3,14 @@ const chatbotDataset = {
     "greeting": "Hello! How can I assist you today?",
     "farewell": "Goodbye! Have a great day!",
     "help": "Of course! What can I help you with?",
+    "creators": "(link to about section page)",
     "no help": "I'm sorry, I am not able to help you with that"
 }
 
 const greetingRegex = /(hello|hi|hey|morning|afternoon|evening)\b/i;
 const farewellRegex = /(bye|see you later|farewell|adios|night)\b/i;
 const helpRegex = /(help)\b/i;
+const creatorsRegex = /(made|created|designed|developed|built)\b(website|page|site|web)?\b/i;
 
 const getResponse = (message) => {
     let response = "";
@@ -16,13 +18,17 @@ const getResponse = (message) => {
     if (greetingRegex.test(message)) {
         response += chatbotDataset["greeting"] + "<br><br>";
     }
-
-    if (farewellRegex.test(message)) {
-        response += chatbotDataset['farewell'] + "<br><br>";
-    }
     
     if (helpRegex.test(message)) {
         response += chatbotDataset['help'] + "<br><br>";
+    }
+
+    if (creatorsRegex.test(message)) {
+        response += chatbotDataset['creators'] + "<br><br>";
+    }
+
+    if (farewellRegex.test(message)) {
+        response += chatbotDataset['farewell'] + "<br><br>";
     }
 
     return response.slice(0, -8);
--- home-page/script.js
+++ home-page/script.js
@@ -1,7 +1,7 @@
 const chatbotWindow = document.getElementById("chatbot-window");
 const body = document.getElementById('messaging-body');
 const textInput = document.getElementById('user-input');
-const messages = [];
+let messages = [];
 const domain = 'http://localhost:3000';
 
 const openChatbotWindow = () => {
@@ -37,19 +37,21 @@ const generateMessage = async() => {
         return;
     }
     
-    // Use this index number method to put gaps between messages
-    messages.push({human: input});
+    await sendMessage(input);
+
+    let AIMessage = await getMessage();
+    messages.push({human: input, ai: AIMessage.message});
+
     body.innerHTML = messages.map((message, index) => {
-        return `<div style="top: ${(index+1)*1.5}em;" class="human-text">${message.human}</div>`;
+        return `
+            <div class="message human-text">
+                <p>${message.human}</p>
+            </div>
+            <div class="message ai-text">
+                <p>${message.ai}</p>
+            </div>`;
     }).join('');
-    //await sendMessage(input);
-
-    //let AIMessage = await getMessage();
-    //messages.push({human: input, ai: AIMessage.message});
-
-    //body.innerHTML = messages.map((message) => {
-    //    return `<p>Human: ${message.human}</p><p style="color: green;">AI: ${message.ai}</p>`;
-    //}).join('');
+    
 
     textInput.value = "";
 }
