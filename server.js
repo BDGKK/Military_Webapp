fb2202cc28fa0417616a9d6df114349c20a387bb
rekredhead <rekredhead@gmail.com>
2023-03-19 18:24:02 +0530

    Hosted all webpages on server
    Connected each webpage router to main express app server.js
    Remove '.' sign in uri of downloadApplication.js

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ require('./database/create_db'); // Create database at initial server setup
 // Import directories of the webpages
 const homepage = require('./server-side/chatbot/homePage');
 const registrationPage = require('./server-side/registration');
-const profilepage = require('./server-side/profilePage');
+const profilePage = require('./server-side/profilePage');
 const forgetPasswordPage = require('./server-side/forgetPassword');
+const downloadApplicationPage = require('./server-side/downloadApplication');
+const loanPage = require('./server-side/loan');
+const needHelpPage = require('./server-side/needHelp');
+const pensionPage = require('./server-side/pension');
+const userLogPage = require('./server-side/userLog');
 
 const app = express();
 
@@ -19,14 +24,19 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     return next();
 });
-app.use(express.json());
 
+app.use(express.json());
 app.use(express.static('./images')); // Host all public images
 
 // Connect the routers of the webpage to the main app
 app.use(homepage);
 app.use(registrationPage);
-app.use(profilepage);
+app.use(profilePage);
 app.use(forgetPasswordPage);
+app.use(downloadApplicationPage);
+app.use(loanPage);
+app.use(needHelpPage);
+app.use(pensionPage);
+app.use(userLogPage);
 
 app.listen(PORT, () => console.log(`Open this link: ${DOMAIN_NAME}`));
