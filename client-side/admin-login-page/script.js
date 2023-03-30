07ac3224373b8c339cb0a71ba8fb9115c710219a
rekredhead <rekredhead@gmail.com>
2023-03-30 14:29:51 +0530

    Admin login page sends admin login data to server to verify user

---
+++ client-side/admin-login-page/script.js
@@ -0,0 +1,34 @@
+const adminEmailEl = document.getElementById("email");
+const adminPasswordEl = document.getElementById("password");
+const loginBtnEl = document.getElementById("login-btn");
+const domain = window.location.origin;
+
+const areInputsValid = () => {
+    // Add input validation here
+    return true;
+}
+
+loginBtnEl.addEventListener('click', async() => {
+    if (!areInputsValid()) return;
+
+    const loginInfo = {
+        email: adminEmailEl.value,
+        password: adminPasswordEl.value
+    }
+
+    const response = await fetch(`${domain}/adminLogin/adminLoginInfo`, {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(loginInfo)
+    });
+
+    if (response.ok) {
+        window.location.href = `${domain}/adminHomepage`;
+    } else {
+        const responseData = await response.json();
+        alert(responseData.message);
+    }
+});
