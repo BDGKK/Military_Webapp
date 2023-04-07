eed1aee58458f488eda1b3631fe47ffd4b6ad454
rekredhead <rekredhead@gmail.com>
2023-04-07 13:18:25 +0530

    Updated forget password page
    New files given by Dilshani
    Removed the form element in html to avoid unintended redirections
    Fixed the checkblank function in javascript
    Added code to send new user data to server

--- client-side/forget-password-page/forget_password.js
+++ client-side/forget-password-page/forget_password.js
@@ -1,11 +1,61 @@
-const newpw = document.getElementById("newpw")
-const cmpw = document.getElementById('cmpw')
-const digit = document.getElementById('digit')
+const userEmail = document.getElementById("uemail");
+const newPassword = document.getElementById("newpw");
+const confirmedPassword = document.getElementById('cmpw');
+const verificationCode = document.getElementById('digit');
 
-function checkblank(){
-    if(newpw.value != cmpw.value)
-    alert('New password and the comfirm password does not match')
+const sendVerificationCodeBtn = document.getElementById('send-vc-btn');
+const setNewPasswordBtn = document.getElementById('set-password-btn');
 
-    if(digit.value == "")
-    alert('Enter the digit number that you got from email')
+const domain = window.location.origin;
+
+function checkblank(x){
+    if (userEmail.value === "") {
+        alert("Enter your email address");
+        return true;
+    }
+    if (newPassword.value === "" || confirmedPassword === "") {
+        alert("Enter your new password");
+        return true;
+    }
+    if(newPassword.value != confirmedPassword.value) {
+        alert('New password and the comfirm password does not match');
+        return true;
+    }
+
+    if (x === 0) return false;
+    if(verificationCode.value == "") {
+        alert('Enter the digit number that you got from email');
+        return true;
+    }
+
+    return false;
 }
+
+sendVerificationCodeBtn.addEventListener('click', async() => {
+    if (checkblank(0)) return;
+
+    const newUserData = {
+        email: userEmail.value,
+        password: newPassword.value
+    }
+
+    const response = await fetch(`${domain}/forget-password/newUserData`, {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUserData)
+    });
+
+    if (response.ok) {
+        alert("Please enter the verification code sent to your email");
+    } else {
+        const responseData = await response.json();
+        alert(responseData.message);
+    }
+});
+
+setNewPasswordBtn.addEventListener('click', () => {
+    if (checkblank(1)) return;
+});
