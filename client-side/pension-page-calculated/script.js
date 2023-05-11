491a069e709de753ea601a16b0ba2a9c9a6dd0b8
rekredhead <rekredhead@gmail.com>
2023-05-11 08:48:37 +0530

    Enabled data to render on calculated pension page

--- client-side/pension-page-calculated/script.js
+++ client-side/pension-page-calculated/script.js
@@ -1,10 +1,32 @@
 const userIDEl          = document.getElementById('user-id');
 const emailAddrEl       = document.getElementById("email-addr");
 const rankEl            = document.getElementById("rank");
-const salaryEl          = document.getElementById("salary").getAttribute("data-value");
-const yearsOfServiceEl  = document.getElementById("years").getAttribute("data-value");
+const salaryEl          = document.getElementById("salary");
+const yearsOfServiceEl  = document.getElementById("years");
 const disabilities      = document.getElementById("disabilities");
 const granterName       = document.getElementById("granter-name");
 const granterRelation   = document.getElementById("granter-relation");
 const renewDateEl       = document.getElementById("renew-date");
 const paymentEl         = document.getElementById("payment");
+
+const domain = window.location.origin;
+
+const loadData = async() => {
+    const userDataResponse = await fetch(`${domain}/profile/userData`);
+    const userData = await userDataResponse.json();
+
+    userIDEl.innerHTML = userData.userId;
+    emailAddrEl.innerHTML = userData.emailAddr;
+    rankEl.innerHTML = userData.rank;
+    yearsOfServiceEl.innerHTML = userData.yearsOfService;
+    salaryEl.innerHTML = userData.salary;
+
+    disabilities.innerHTML = localStorage.getItem('disabilities') || null;
+    granterName.innerHTML = localStorage.getItem('granterName') || null;
+    granterRelation.innerHTML = localStorage.getItem('granterRelationship') || null;
+    renewDateEl.innerHTML = localStorage.getItem('renewalDate') || null;
+    paymentEl.innerHTML = localStorage.getItem('monthlyPayment') || null;
+
+    localStorage.clear();
+}
+loadData();
