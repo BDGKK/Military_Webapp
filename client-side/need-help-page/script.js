ca8a914ae0f2e8c85a3f8e8913c7d6528f1934af
rekredhead <rekredhead@gmail.com>
2023-05-18 11:25:08 +0530

    Changed design of help desk page

--- client-side/need-help-page/script.js
+++ client-side/need-help-page/script.js
@@ -1,20 +1,2 @@
-const registryPageLink = document.getElementById('registration-page-link');
-const forgetPasswordPageLink = document.getElementById('forget-password-page-link');
-const pensionPageLink = document.getElementById('pension-page-link');
-const loanPageLink = document.getElementById('loan-page-link');
-
 const domain = window.location.origin;
-document.querySelector('head').innerHTML += `<link rel="icon" type="image/x-icon" href="${domain}/logo.png">`;
-
-registryPageLink.addEventListener('click', () => {
-    registryPageLink.href = `${domain}/registration`;
-});
-forgetPasswordPageLink.addEventListener('click', () => {
-    forgetPasswordPageLink.href = `${domain}/forget-password`;
-});
-pensionPageLink.addEventListener('click', () => {
-    pensionPageLink.href = `${domain}/pension`;
-});
-loanPageLink.addEventListener('click', () => {
-    loanPageLink.href = `${domain}/loan`;
-});
+document.querySelector('.column1').innerHTML = `<img src="${domain}/logo.png" height="180" alt="logo">`;
